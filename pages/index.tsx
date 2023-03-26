9003dee586337646eb06e04b026123b078448386
bouahth2018 <bouahth2018@gmail.com>
2023-03-26 20:20:26 +0700

    Update Title in Head tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import { Layout, Text, Page } from "@vercel/examples-ui";
 import { Chat } from "../components/Chat";
+import Head from "next/head";
 
 function Home() {
   return (
-    <div className="h-screen bg-[#111]">
-      <Chat />
+    <div>
+      <Head>
+        <title>Luna AI Powered by ChatGPT</title>
+      </Head>
+      <div className="h-screen bg-[#111]">
+        <Chat />
+      </div>
     </div>
   );
 }
