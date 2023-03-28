6fdc27787e375630112d1602a76e0d2b31915147
bouahth2018 <bouahth2018@gmail.com>
2023-03-28 15:37:28 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('@vercel/examples-ui/tailwind')],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@vercel/examples-ui/**/*.js',
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
   ],
 }
